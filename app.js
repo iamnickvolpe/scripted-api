fb4d4062eb757162c4773801c7d2dba8e53a300b
Nick Volpe <nvolpe@nvolpe-ltm1.internal.salesforce.com>
2018-03-20 21:22:29 -0400

    Removed stuff.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -14,8 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,7 +27,6 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
